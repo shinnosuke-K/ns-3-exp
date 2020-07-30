9ff4632ab880d361a919302b7bfea04010ee5b81
shinnosuke-K <shin.050195@gmail.com>
2020-07-30 17:42:51 +0900

    edit

--- exp.cc
+++ exp.cc
@@ -121,10 +121,13 @@ main (int argc, char *argv[])
   // ipv4h.SetBase ("1.0.0.0", "255.0.0.0");
   // Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign (internetDevices);
   // interface 0 is localhost, 1 is the p2p device
+
+  std::cout << 0 << std::endl;
+
+
   Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress (1);
 
   Ipv4StaticRoutingHelper ipv4RoutingHelper;
-  std::cout << 0 << std::endl;
 
   Ptr<Ipv4StaticRouting> remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting (remoteHost->GetObject<Ipv4> ());
   remoteHostStaticRouting->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"), 1);
