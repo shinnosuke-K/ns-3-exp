d1b89b136fb2eaae31b4cb854ca9560071ff7e93
shinnosuke-K <shin.050195@gmail.com>
2020-07-30 16:29:14 +0900

    edit

--- exp.cc
+++ exp.cc
@@ -1,6 +1,6 @@
 /* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2011, 2013 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ * Copyright (c) 2011-2018 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,184 +15,92 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Jaume Nin <jaume.nin@cttc.cat>
- *         Nicola Baldo <nbaldo@cttc.cat>
+ * Author: Manuel Requena <manuel.requena@cttc.es>
  */
 
-#include "ns3/lte-helper.h"
-#include "ns3/epc-helper.h"
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
-#include "ns3/ipv4-global-routing-helper.h"
-#include "ns3/internet-module.h"
 #include "ns3/mobility-module.h"
 #include "ns3/lte-module.h"
-#include "ns3/applications-module.h"
-#include "ns3/point-to-point-helper.h"
-#include <ns3/config-store-module.h>
+#include "ns3/config-store.h"
+#include <ns3/buildings-helper.h>
+//#include "ns3/gtk-config-store.h"
 
 using namespace ns3;
 
-/*
- * Simple simulation program using the emulated EPC.
- * For the LTE radio part, it simulates a simple linear topology with
- * a fixed number of eNBs spaced at equal distance, and a fixed number
- * of UEs per each eNB, located at the same position of the eNB.
- * For the EPC, it uses EmuEpcHelper to realize the S1-U connection
- * via a real link.
- */
-
-NS_LOG_COMPONENT_DEFINE ("EpcFirstExample");
-
-int
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
+  Time simTime = MilliSeconds (1050);
+  bool useCa = false;
 
-  uint16_t nEnbs = 1;
-  uint16_t nUesPerEnb = 1;
-  double simTime = 10.1;
-  double distance = 1000.0;
-  double interPacketInterval = 1000;
-
-  // Command line arguments
   CommandLine cmd;
-  cmd.AddValue("nEnbs", "Number of eNBs", nEnbs);
-  cmd.AddValue("nUesPerEnb", "Number of UEs per eNB", nUesPerEnb);
-  cmd.AddValue("simTime", "Total duration of the simulation [s])", simTime);
-  cmd.AddValue("distance", "Distance between eNBs [m]", distance);
-  cmd.AddValue("interPacketInterval", "Inter packet interval [ms])", interPacketInterval);
-  cmd.Parse(argc, argv);
+  cmd.AddValue ("simTime", "Total duration of the simulation", simTime);
+  cmd.AddValue ("useCa", "Whether to use carrier aggregation.", useCa);
+  cmd.Parse (argc, argv);
 
-  // let's go in real time
-  // NOTE: if you go in real time I strongly advise to use
-  // --ns3::RealtimeSimulatorImpl::SynchronizationMode=HardLimit
-  // I've seen that if BestEffort is used things can break
-  // (even simple stuff such as ARP)
-  //GlobalValue::Bind ("SimulatorImplementationType",
-  //                 StringValue ("ns3::RealtimeSimulatorImpl"));
-
- // let's speed things up, we don't need these details for this scenario
-  Config::SetDefault ("ns3::LteSpectrumPhy::CtrlErrorModelEnabled", BooleanValue (false));
-  Config::SetDefault ("ns3::LteSpectrumPhy::DataErrorModelEnabled", BooleanValue (false));
-
-  GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
+  // to save a template default attribute file run it like this:
+  // ./waf --command-template="%s --ns3::ConfigStore::Filename=input-defaults.txt --ns3::ConfigStore::Mode=Save --ns3::ConfigStore::FileFormat=RawText" --run src/lte/examples/lena-simple
+  //
+  // to load a previously created default attribute file
+  // ./waf --command-template="%s --ns3::ConfigStore::Filename=input-defaults.txt --ns3::ConfigStore::Mode=Load --ns3::ConfigStore::FileFormat=RawText" --run src/lte/examples/lena-simple
 
   ConfigStore inputConfig;
-  inputConfig.ConfigureDefaults();
+  inputConfig.ConfigureDefaults ();
 
-  // parse again so you can override default values from the command line
-  cmd.Parse(argc, argv);
+  // Parse again so you can override default values from the command line
+  cmd.Parse (argc, argv);
 
-
+  if (useCa)
+   {
+     Config::SetDefault ("ns3::LteHelper::UseCa", BooleanValue (useCa));
+     Config::SetDefault ("ns3::LteHelper::NumberOfComponentCarriers", UintegerValue (2));
+     Config::SetDefault ("ns3::LteHelper::EnbComponentCarrierManager", StringValue ("ns3::RrComponentCarrierManager"));
+   }
 
   Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
-  Ptr<EmuEpcHelper>  epcHelper = CreateObject<EmuEpcHelper> ();
-  lteHelper->SetEpcHelper (epcHelper);
-  epcHelper->Initialize ();
 
-  Ptr<Node> pgw = epcHelper->GetPgwNode ();
+  // Uncomment to enable logging
+//  lteHelper->EnableLogComponents ();
 
-   // Create a single RemoteHost
-  NodeContainer remoteHostContainer;
-  remoteHostContainer.Create (1);
-  Ptr<Node> remoteHost = remoteHostContainer.Get (0);
-  InternetStackHelper internet;
-  internet.Install (remoteHostContainer);
-
-  // Create the Internet
-  PointToPointHelper p2ph;
-  p2ph.SetDeviceAttribute ("DataRate", DataRateValue (DataRate ("100Gb/s")));
-  p2ph.SetDeviceAttribute ("Mtu", UintegerValue (1500));
-  p2ph.SetChannelAttribute ("Delay", TimeValue (Seconds (0.010)));
-  NetDeviceContainer internetDevices = p2ph.Install (pgw, remoteHost);
-  Ipv4AddressHelper ipv4h;
-  ipv4h.SetBase ("1.0.0.0", "255.0.0.0");
-  Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign (internetDevices);
-  // interface 0 is localhost, 1 is the p2p device
-  Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress (1);
-
-  Ipv4StaticRoutingHelper ipv4RoutingHelper;
-  Ptr<Ipv4StaticRouting> remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting (remoteHost->GetObject<Ipv4> ());
-  remoteHostStaticRouting->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"), 1);
-
-  NodeContainer ueNodes;
+  // Create Nodes: eNodeB and UE
   NodeContainer enbNodes;
-  enbNodes.Create(nEnbs);
-  ueNodes.Create(nEnbs*nUesPerEnb);
+  NodeContainer ueNodes;
+  enbNodes.Create (1);
+  ueNodes.Create (1);
 
   // Install Mobility Model
-  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-  for (uint16_t i = 0; i < nEnbs; i++)
-    {
-      positionAlloc->Add (Vector(distance * i, 0, 0));
-    }
   MobilityHelper mobility;
-  mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-  mobility.SetPositionAllocator(positionAlloc);
-  mobility.Install(enbNodes);
-  mobility.Install(ueNodes);
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install (enbNodes);
+  BuildingsHelper::Install (enbNodes);
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install (ueNodes);
+  BuildingsHelper::Install (ueNodes);
 
-  // Install LTE Devices to the nodes
-  NetDeviceContainer enbLteDevs = lteHelper->InstallEnbDevice (enbNodes);
-  NetDeviceContainer ueLteDevs = lteHelper->InstallUeDevice (ueNodes);
+  // Create Devices and install them in the Nodes (eNB and UE)
+  NetDeviceContainer enbDevs;
+  NetDeviceContainer ueDevs;
+  // Default scheduler is PF, uncomment to use RR
+  //lteHelper->SetSchedulerType ("ns3::RrFfMacScheduler");
 
-  // Install the IP stack on the UEs
-  internet.Install (ueNodes);
-  Ipv4InterfaceContainer ueIpIface;
-  ueIpIface = epcHelper->AssignUeIpv4Address (NetDeviceContainer (ueLteDevs));
-  // Assign IP address to UEs, and install applications
-  for (uint32_t u = 0; u < ueNodes.GetN (); ++u)
-    {
-      Ptr<Node> ueNode = ueNodes.Get (u);
-      // Set the default gateway for the UE
-      Ptr<Ipv4StaticRouting> ueStaticRouting = ipv4RoutingHelper.GetStaticRouting (ueNode->GetObject<Ipv4> ());
-      ueStaticRouting->SetDefaultRoute (epcHelper->GetUeDefaultGatewayAddress (), 1);
-    }
+  enbDevs = lteHelper->InstallEnbDevice (enbNodes);
+  ueDevs = lteHelper->InstallUeDevice (ueNodes);
 
-  lteHelper->Attach (ueLteDevs);
-  // side effects: 1) use idle mode cell selection, 2) activate default EPS bearer
+  // Attach a UE to a eNB
+  lteHelper->Attach (ueDevs, enbDevs.Get (0));
 
-  // randomize a bit start times to avoid simulation artifacts
-  // (e.g., buffer overflows due to packet transmissions happening
-  // exactly at the same time)
-  Ptr<UniformRandomVariable> startTimeSeconds = CreateObject<UniformRandomVariable> ();
-  startTimeSeconds->SetAttribute ("Min", DoubleValue (0));
-  startTimeSeconds->SetAttribute ("Max", DoubleValue (interPacketInterval/1000.0));
+  // Activate a data radio bearer
+  enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
+  EpsBearer bearer (q);
+  lteHelper->ActivateDataRadioBearer (ueDevs, bearer);
+  lteHelper->EnableTraces ();
 
-  // Install and start applications on UEs and remote host
-  uint16_t dlPort = 1234;
-  uint16_t ulPort = 2000;
-  for (uint32_t u = 0; u < ueNodes.GetN (); ++u)
-    {
-      ++ulPort;
-      ApplicationContainer clientApps;
-      ApplicationContainer serverApps;
+  Simulator::Stop (simTime);
+  Simulator::Run ();
 
+  // GtkConfigStore config;
+  // config.ConfigureAttributes ();
 
-      PacketSinkHelper dlPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), dlPort));
-      PacketSinkHelper ulPacketSinkHelper ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), ulPort));
-      serverApps.Add (dlPacketSinkHelper.Install (ueNodes.Get(u)));
-      serverApps.Add (ulPacketSinkHelper.Install (remoteHost));
-
-      UdpClientHelper dlClient (ueIpIface.GetAddress (u), dlPort);
-      dlClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
-      dlClient.SetAttribute ("MaxPackets", UintegerValue(1000000));
-
-      UdpClientHelper ulClient (remoteHostAddr, ulPort);
-      ulClient.SetAttribute ("Interval", TimeValue (MilliSeconds(interPacketInterval)));
-      ulClient.SetAttribute ("MaxPackets", UintegerValue(1000000));
-
-      clientApps.Add (dlClient.Install (remoteHost));
-      clientApps.Add (ulClient.Install (ueNodes.Get(u)));
-
-      serverApps.Start (Seconds (startTimeSeconds->GetValue ()));
-      clientApps.Start (Seconds (startTimeSeconds->GetValue ()));
-    }
-
-  Simulator::Stop(Seconds(simTime));
-  Simulator::Run();
-
-  Simulator::Destroy();
+  Simulator::Destroy ();
   return 0;
 }
-
