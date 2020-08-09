c36164b341c51a7869b3e799dbbd654bc5173364
shinnosuke-K <shin.050195@gmail.com>
2020-08-09 13:44:48 +0900

    edit

--- exp.cc
+++ exp.cc
@@ -127,6 +127,8 @@ main (int argc, char *argv[])
   //   // std::cout << remoteHostAddr << std::endl;
   // }
 
+  std::cout << 0 << std::endl;
+
   internetDevices = p2ph.Install (pgw, remoteHost);
 
   // Ipv4AddressHelper ipv4h;
@@ -134,14 +136,15 @@ main (int argc, char *argv[])
   internetIpIfaces = ipv4h.Assign (internetDevices);
   // interface 0 is localhost, 1 is the p2p device
 
+  std::cout << 1 << std::endl;
+
 
   Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress (1);
-
   Ipv4StaticRoutingHelper ipv4RoutingHelper;
-
   Ptr<Ipv4StaticRouting> remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting (remoteHost->GetObject<Ipv4> ());
   remoteHostStaticRouting->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"), 1);
 
+  std::cout << 2 << std::endl;
 
 
   NodeContainer ueNodes;
@@ -160,6 +163,9 @@ main (int argc, char *argv[])
   mobility.SetPositionAllocator(positionAlloc);
   mobility.Install(enbNodes);
   mobility.Install(ueNodes);
+
+  std::cout << 4 << std::endl;
+
 
   // Install LTE Devices to the nodes
   NetDeviceContainer enbLteDevs = lteHelper->InstallEnbDevice (enbNodes);
